f5502a44ff3871de07035c6c52448ee6e86721e9
Hideyuki Miyahara <hmiyahara512@gmail.com>
2014-08-30 14:33:43 +0900

    7th commit

--- cat.hs
+++ cat.hs
@@ -1,7 +1,25 @@
 import System.Environment
+import System.IO
 
-main = getArgs >>= \filenames ->
-     mapM readFile filenames >>=
+cat :: [String] -> IO ()
+cat args = if args== []
+    then catStdin
+    else catFile args
+
+catFile :: [String] -> IO ()
+catFile p = (\fnames ->
+     mapM readFile fnames) p >>=
      mapM_ (\(x) ->
            putStrLn $ concat [
            x])
+
+catStdin :: IO ()
+catStdin = useLineBuffering >> interact id
+
+useLineBuffering :: IO ()
+useLineBuffering = hSetBuffering stdout LineBuffering
+
+main :: IO ()
+main = do
+     args <- getArgs
+     cat args
